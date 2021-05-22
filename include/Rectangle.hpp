3494540c5d77c1c4b12227002f7cf1e5e0b496d8
Mathieu R <mathieu.roger@epitech.eu>
2021-05-23 00:13:51 +0200

    [+] Rectangle Class : Ctor / dtor / draw / collision

---
+++ include/Rectangle.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2020
+** YEP Indie Studio
+** File description:
+** Raylib Rectangle Class
+*/
+
+#ifndef RL_RECTANLE_
+#define RL_RECTANLE_
+
+#include "rayLib.hpp"
+
+namespace rl {
+
+class Color;
+
+class Rectangle : public ::Rectangle
+{
+    public:
+        Rectangle(float x, float y, float width, float height);
+        Rectangle(const Rectangle &rect);
+        ~Rectangle() {};
+
+        void draw(rl::Color color);
+        void drawLine(rl::Color color);
+
+        const Rectangle getCollisionRec(rl::Rectangle rect);
+    private:
+};
+
+}
+
+#endif
---
+++ src/raylib/Rectangle.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2020
+** YEP Indie Studio
+** File description:
+** Raylib Rectangle Functions
+*/
+
+#include "Rectangle.hpp"
+
+rl::Rectangle::Rectangle(float x = 0, float y = 0, float width = 0, float height = 0)
+    : ::Rectangle{x, y, width, height}
+{
+}
+
+rl::Rectangle::Rectangle(const rl::Rectangle &rect)
+    : ::Rectangle{rect.x, rect.y, rect.width, rect.height}
+{
+}
+
+void rl::Rectangle::draw(rl::Color color)
+{
+    DrawRectangle(x, y, width, height, color);
+}
+
+void rl::Rectangle::drawLine(rl::Color color)
+{
+    DrawRectangleLines(x, y, width, height, color);
+}
+
+const rl::Rectangle rl::Rectangle::getCollisionRec(rl::Rectangle rect)
+{
+    ::Rectangle newRect = GetCollisionRec(*this, rect);
+    return rl::Rectangle(newRect.x, newRect.y, newRect.width, newRect.height);
+}
